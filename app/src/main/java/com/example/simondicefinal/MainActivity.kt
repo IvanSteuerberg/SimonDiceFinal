cb82451ae4e580cf759245cdd4cd1e5c090a3b79
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2020-12-05 17:59:45 +0100

    Creado ViewModel e implementada visualización de secuencia en Main (botones start y botones de colores funcionando), utilización de corrutina

--- app/src/main/java/com/example/simondicefinal/MainActivity.kt
+++ app/src/main/java/com/example/simondicefinal/MainActivity.kt
@@ -1,11 +1,68 @@
 package com.example.simondicefinal
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val miModelo by viewModels<MyViewModel>()
+
+        val start = findViewById<Button>(R.id.start_button)
+        val check = findViewById<Button>(R.id.check_button)
+
+        val blue = findViewById<Button>(R.id.blue_button)
+        val red = findViewById<Button>(R.id.red_button)
+        val yellow = findViewById<Button>(R.id.yellow_button)
+        val green = findViewById<Button>(R.id.green_button)
+
+        val listaBotones = listOf(green,red,blue,yellow)
+
+        miModelo.listaReto.observe(this, Observer{
+            miModelo.mostrarSecuencia(listaBotones)
+        })
+
+        start.setOnClickListener{
+            miModelo.resetear()
+            miModelo.añadirValor()
+        }
+
+        check.setOnClickListener{
+
+        }
+
+        green.setOnClickListener{
+            miModelo.guardarSecuencia(miModelo.listaJugador,1)
+        }
+
+        red.setOnClickListener{
+            miModelo.guardarSecuencia(miModelo.listaJugador,2)
+        }
+
+        blue.setOnClickListener{
+            miModelo.guardarSecuencia(miModelo.listaJugador,3)
+        }
+
+        yellow.setOnClickListener{
+            miModelo.guardarSecuencia(miModelo.listaJugador,4)
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/simondicefinal/MyViewModel.kt
@@ -0,0 +1,70 @@
+package com.example.simondicefinal
+
+
+
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.widget.Button
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+
+import kotlin.random.Random
+
+class MyViewModel : ViewModel() {
+
+    var listaReto = MutableLiveData<MutableList<Int>>()
+    val listaJugador = MutableLiveData<MutableList<Int>>()
+
+    init {
+        listaReto.value = mutableListOf()
+        listaJugador.value = mutableListOf()
+    }
+
+    fun añadirValor() {
+        val numero = Random.nextInt(4) + 1
+        listaReto.value?.add(numero)
+        listaReto.postValue(listaReto.value)
+    }
+
+    fun guardarSecuencia(listaJugador: MutableLiveData<MutableList<Int>>, number: Int) {
+        when (number) {
+            1 -> listaJugador.value!!.add(1)
+            2 -> listaJugador.value!!.add(2)
+            3 -> listaJugador.value!!.add(3)
+            else -> listaJugador.value!!.add(4)
+        }
+    }
+
+    fun resetear() {
+        listaReto.value!!.clear()
+        listaJugador.value!!.clear()
+    }
+
+    fun mostrarSecuencia(listaBotones: List<Button>) {
+        CoroutineScope(Dispatchers.Main).launch {
+            for (colors in listaReto.value!!) {
+                listaBotones.get(colors-1).setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFFFFF")))
+                delay(1000)
+                when (colors){
+                    1-> listaBotones.get(colors-1).setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#00C853")))
+                    2-> listaBotones.get(colors-1).setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#D50000")))
+                    3-> listaBotones.get(colors-1).setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#0091EA")))
+                    4-> listaBotones.get(colors-1).setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFD600")))
+                }
+            }
+        }
+
+
+    }
+
+
+
+
+
+
+}
